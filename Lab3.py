40683330d532861e371584e6965425b02b3392ad
D19124970 Donal Lynch <D19124970@mydit.ie>
2019-10-01 20:58:41 +0100

    lab finished

--- Lab3.py
+++ Lab3.py
@@ -69,6 +69,7 @@
 # Excercise 7
 ##i = 0
 ##while i >= 0:
+##    while(i<0) or (
 ##    i = int(input('Please enter a number: '))
 ##    print('you entered', i)
 ##print('Goodbye')
@@ -85,11 +86,66 @@
 
 
 # Excercise 9
-for i in range(1, 6):
-    for j in range(1, i+1):
-        print(j, end = '')
-    print('')
+##for i in range(1, 6):
+##    for j in range(1, i+1):
+##        print(j, end = '')
+##    print('')
 
                 
+### Excercises from w3resources Python basic part II
+
+##secret_number = 10
+##guess = int(input('please guess a number: '))
+##counter = 1
+##
+##while guess != secret_number:
+##    if guess > secret_number:
+##        guess = int(input('Too high, guess again: '))
+##    else:
+##        guess = int(input('Too low, guess again: '))
+##
+##    counter+=1
+##
+##print('well done, it took', counter, 'guesses')
+    
+
+### REmember: The , character after our print statement means that our next print statement
+### keeps printing on the same line. Let's filter out the letter 'A' from our string.
+##
+##phrase = "A bird in the hand..."
+##
+##for l in phrase:
+##    if 'A' == l:
+##        print('X', end = '')
+##    elif 'a' == l:
+##        print('x', end = '')
+##    else:
+##        print(l, end = '')
+
+
+score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2,
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3,
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1,
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4,
+         "x": 8, "z": 10}
+
+
+word = input('Please enter a number: ')
+total = 0
+for l in word:
+    total+=score[l]
+
+print(total)
+
+
+
+
+
+
+
+
+
+
+
 
                        
