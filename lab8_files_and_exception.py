133b11ce8238a8773ce57110ecaf5ee73f0b14e3
D19124970 Donal Lynch <D19124970@mydit.ie>
2019-11-19 20:48:25 +0000

    tidied up

--- lab8_files_and_exception.py
+++ lab8_files_and_exception.py
@@ -1,48 +1,23 @@
-'''
-input_file = open('twinkle.txt', 'r')
-
-for line in input_file:
-    print(line)
-
-input_file.close()
-
-
-#example from note pg 19
-out_file = open('numbers.txt', 'w')
-
-for i in range(10):
-    out_file.write(str(i))
-    out_file.write('\n')
-out_file.close()
-
-
-
-
-while True:
-    num = input('enter a number: ')
-
-    try:
-        int_num = int(num)
-        print('your number is', str(int_num))
-    except ValueError:
-        print('that aint no number')
-'''
-
 print('\n ==================== Excercise 1 ==================== \n')
 # Exercise 1: Write a Python program that opens a text file, # reads the contents line by line and saves it into
 # a second file.
 # Make sure you use try/except to handle and possible problems with incorrect filenames.
-with open('junk.txt', 'r') as write_file:
-    with open('twinkle.txt', 'r') as input_file:
-        for line in input_file:
-            try:
-                write_file.write(line[:10])
-            except IOError:
-                print('major file error')
-                break
-            else:
-                if line != '\n':  # Dont print a new line if the line is a blank line
-                    write_file.write('\n')
+
+try:
+    write_file = open('junk.txt', 'r')
+    input_file = open('twinkle.txt', 'r')
+except IOError:
+    print('could not open files')
+else:
+    for line in input_file:
+        try:
+            write_file.write(line[:10])
+        except IOError:
+            print('do not have permission to write')
+            break
+        else:
+            if line != '\n':  # Dont print a new line if the line is a blank line
+                write_file.write('\n')
 
 print('\n ==================== Excercise 2 ==================== \n')
 # Exercise 2: Write a Python program that opens a text file, reads the contents line
@@ -63,4 +38,4 @@ print('\n ==================== Excercise 4 ==================== \n')
 # Exercise 4: Write a Python program that reads a text file and prints the length of each line.
 with open('twinkle.txt', 'r') as input_file:
     for line in input_file:
-        print(len(line))
+        print(str(len(line)))
