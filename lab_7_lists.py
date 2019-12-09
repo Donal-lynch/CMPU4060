9d9cbe3a227e9681fe4fe9dc59794b1823ac9031
Donal-lynch <lynchdo@tcd.ie>
2019-12-09 17:38:24 +0000

    added functions to lab 6 as required by questions

--- lab_7_lists.py
+++ lab_7_lists.py
@@ -1,61 +1,75 @@
 # Exercise 1: Write a Python function to sum all numbers in a list
+print ('*************excercise 1 *******************\n')
+
+def list_sum(my_list):
+    counter = 0
+
+    for i in my_list:
+        counter += i
+    return counter
+
 my_list = [1, 2, 3, 4, 5, 7, 6]
+print(list_sum(my_list))
 
-counter = 0
-
-for el in my_list:
-    counter += el
-
-print(counter)
-
+print ('\n*************excercise 2 *******************\n')
 
 # Exercise 2: Write a Python function to get the largest number from a list.
-print(max(my_list))
+def list_max(my_list):
+    max_element = my_list[0]
 
+    for i in my_list:
+        if i > max_element:
+            max_element = i
 
+    return max_element
+
+print(list_max(my_list))
+
+print ('\n*************excercise 3 *******************\n')
 # Exercise 3  Write a Python function that takes a list of words and counts how many
 # of them begin with ‘a’
-my_list = 'Lorem ipsum dolor sit amet aonsectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'.split(' ')
 
-counter = 0
-for word in my_list:
-    if word[0].lower() == 'a':
-        counter += 1
-print(counter, 'words starting with "a"')
+def a_counter(word_list):
+    counter = 0
+    for word in word_list:
+        if word[0] == 'a':
+            counter += 1
+    return counter
+
+# make a word list. NOTE split at the end converts string to list
+word_list = 'Lorem ipsum dolor sit amet aonsectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'.split(' ')
+print(a_counter(word_list), 'words starting with "a"')
 
 
-
+print ('\n*************excercise 4 *******************\n')
 # Exercise 4: (modify Ex3)
 # Write a Python function that takes a list of words and a character, and counts how
 # many of the words in the list begin with that character.
 
-my_list = 'Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore' \
-          ' magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat' \
-          ' Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore' \
-          ' eu fugiat nulla pariatur Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum'.split(' ')
+
+def letter_counter(word_list, letter):
+    counter = 0
+    for word in word_list:
+        if word[0] == letter:
+            counter += 1
+    return counter
+
 
 first_letter = input('enter a first letter: ')
-
-counter = 0
-for word in my_list:
-    if word[0].lower() == first_letter.lower():
-        counter += 1
-print(counter, 'words starting with', first_letter.lower())
+print(letter_counter(word_list, first_letter), 'words starting with', first_letter)
 
 
-
-
+print ('\n*************excercise 5 *******************\n')
 
 # Exercise 5:
 # Write a Python function that takes a list of numbers and returns a
 # new list containing only the even numbers from the first list
 
-my_list = [1, 2, 3, 4, 5, 7, 6]
-new_list = []
+def even_checker(my_list):
+    new_list = []
 
-for i in my_list:
-    if i % 2 == 0:
-        new_list.append(i)
-print(new_list)
-
-
+    for i in my_list:
+        if i % 2 == 0:
+            new_list.append(i)
+    return new_list
+print(even_checker(my_list))
